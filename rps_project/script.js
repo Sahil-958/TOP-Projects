fc58b28a579697fbc08a63b4bfd00847f3679ce4
Sahil <118348625+Sahil-958@users.noreply.github.com>
2023-08-08 12:56:52 +0530

    Extensive Commit: Holds Initial JS Implementaiton

--- rps_project/script.js
+++ rps_project/script.js
@@ -3,41 +3,91 @@ const SCISSORS = "scissors";
 const PAPER = "paper";
 
 let choices = [ROCK, SCISSORS, PAPER];
-
+let userScore = 0;
+let computerScore = 0;
+let roundCount=0;
 let playerSelection = "rock";
-console.log("Welcome to Console Rock Paper Scissors\nTo start Type: game(); into tha console and press ENTER")
+const user = document.querySelector("#user");
+user.textContent = `You: ${userScore}`;
+const computer = document.querySelector("#computer");
+computer.textContent = `Computer: ${computerScore}`;
+const para = document.querySelector(".reason");
+para.textContent = "Welcome to Rock Paper Scissors\nTo Start the game make you choice";
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        para.textContent=`ROUND:${roundCount+1} of 5`;
+        setButton(true);
+        play(button.id, getComputerChoice());
+    });
+});
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
 function play(playerSelection, computerSelection) {
     if ((playerSelection != ROCK) && (playerSelection != SCISSORS) && (playerSelection != PAPER)) {
-        return `BAD Input: Only Rock Paper Scissors | Case insensitive are allowed`;
+        para.textContent = `BAD Input: Only Rock Paper Scissors | Case insensitive are allowed`;
     }
     computerSelection = computerSelection.toLowerCase();
     playerSelection = playerSelection.toLowerCase();
     if (computerSelection === playerSelection) {
-        return "Its a TIE!";
+        setTimeout(() => {
+            para.textContent = "Its a TIE!";
+        }, 500);
     }
     else if (
         (computerSelection == ROCK && playerSelection == SCISSORS) ||
         (computerSelection == SCISSORS && playerSelection == PAPER) ||
         (computerSelection == PAPER && playerSelection == ROCK)
     ) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        setTimeout(() => {
+            para.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        }, 500);
+        ++computerScore;
+        computer.textContent = `Computer: ${computerScore}`;
     }
 
-    else
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-
-
+    else {
+        setTimeout(() => {
+            para.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        }, 500);
+        ++userScore;
+        user.textContent = `You: ${userScore}`;
+    }
+    ++roundCount;
+    if(roundCount === 5) {
+        setTimeout(() => {
+            restartGame();
+            return;
+        }, 1000);
+    }
+    setTimeout(() => {
+        setButton(false);
+    }, 800);
 }
-function game() {
-    console.log("ROCK PAPER SCISSORS ");
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1} of 5:`);
-        playerSelection = prompt("Enter your choice:", playerSelection);
-        console.log(play(playerSelection, getComputerChoice()));
-    }
+            
 
+
+function restartGame(){
+    roundCount=0;
+    if(userScore<computerScore){
+            para.textContent="Beaten By a Peice of Code 💀";
+    }else if(userScore>computerScore){
+            para.textContent="Annd You are worrying About AI taking over your Job";
+    }else {
+            para.textContent="TIE! Let's Play a TIE-BREAKER";
+    }
+    userScore=computerScore=0;
+    setTimeout(() => {
+        window.alert('Round Ended!');
+    }, 1000);
+}
+
+function setButton(status){
+    buttons.forEach(button=>{
+        button.classList.toggle('disabled');
+        button.disabled=status;
+    });
 }
