089188e841d126f77a413f4037d7eaaf53ec1160
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-09-23 13:23:09 +0530

    major refactoring

--- resmaker/src/App.jsx
+++ resmaker/src/App.jsx
@@ -1,8 +1,15 @@
-import { Fragment, useState } from "react";
+import { useState, Fragment, useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-import { FaUser, FaLink, FaTrash } from "react-icons/fa";
-import { UserIcon } from "@heroicons/react/20/solid";
+import {
+  FaUser,
+  FaLink,
+  FaGraduationCap,
+  FaBriefcase,
+  FaTrash,
+  FaLaptopCode,
+} from "react-icons/fa";
+
 import {
   Card,
   Slider,
@@ -36,174 +43,270 @@ function Main({ previewSize, children }) {
   );
 }
 
-function Page({ pageIdx }) {
-  const pages = [
-    <PersonalDetails />,
-    <Links />,
-    <Education />,
-    //<Experience />,
-    //<Skills />,
-    //<Projects />,
+// Reusable Details component for consistent structure
+function Details({ heading, subheading, icon: Icon, children }) {
+  return (
+    <>
+      <CardHeader className="flex gap-3">
+        <Icon className="icon size-8" />
+        <div className="flex flex-col">
+          <h1 className="text-md">{heading}</h1>
+          <p className="text-small text-default-500">{subheading}</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody className="overflow-y-auto">{children}</CardBody>
+    </>
+  );
+}
+
+function DetailContent({ fields }) {
+  return (
+    <>
+      {fields.map((field, idx) => (
+        <div
+          key={idx}
+          className="flex w-full flex-wrap md:flex-nowrap mb-4 gap-4"
+        >
+          {field.map(({ type, label, required, clearable, ...props }, i) => (
+            <Input
+              key={i}
+              type={type}
+              label={label}
+              isRequired={required}
+              {...props}
+            />
+          ))}
+        </div>
+      ))}
+    </>
+  );
+}
+
+function PersonalDetails() {
+  const personalFields = [
+    [
+      { type: "text", label: "Full Name", required: true },
+      { type: "text", label: "Job Title" },
+    ],
+    [
+      { type: "email", label: "Email", required: true },
+      { type: "tel", label: "Phone" },
+    ],
+    [{ type: "text", label: "Address" }],
   ];
 
-  function PersonalDetails() {
-    return (
-      <>
-        <CardHeader className="flex gap-3">
-          <FaUser className="icon size-8" />
-          <div className="flex flex-col">
-            <h1 className="text-md">Personal Details</h1>
-            <p className="text-small text-default-500">
-              Input your personal details
-            </p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody className="overflow-y-auto">
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input type="text" label="Full Name" isRequired isClearable />
-            <Input type="text" label="Job Title" />
-          </div>
-          <Spacer y={4} />
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input type="email" label="Email" isRequired isClearable />
-            <Input type="tel" label="Phone" isClearable />
-          </div>
-          <Spacer y={4} />
-          <Input type="text" label="Address" isClearable />
-          <Spacer y={4} />
-          <Textarea
-            label="Summary"
-            minRows={2}
-            maxRows={4}
-            placeholder="Write a short summary about yourself."
-          />
-        </CardBody>
-      </>
-    );
-  }
+  return (
+    <Details
+      heading="Personal Details"
+      subheading="Input your personal details"
+      icon={FaUser}
+    >
+      <DetailContent fields={personalFields} />
+      <Textarea
+        label="Summary"
+        minRows={2}
+        maxRows={4}
+        placeholder="Write a short summary about yourself."
+      />
+    </Details>
+  );
+}
+// General function to handle changes
+const handleInputChange = (id, field, value, setState) => {
+  setState((prevItems) =>
+    prevItems.map((item) =>
+      item.id === id ? { ...item, [field]: value } : item,
+    ),
+  );
+};
 
-  function Links() {
-    const [links, setLinks] = useState([
-      { id: Date.now() + uuidv4(), icon: "", url: "", label: "" },
-      { id: Date.now() + uuidv4(), icon: "", url: "", label: "" },
-      { id: Date.now() + uuidv4(), icon: "", url: "", label: "" },
-      { id: Date.now() + uuidv4(), icon: "", url: "", label: "" },
-    ]);
+// Function to add a new item
+const handleAddNewItem = (setState, newItem) => {
+  setState((prevItems) => [...prevItems, newItem]);
+};
 
-    function removeLink(id) {
-      setLinks(links.filter((link) => link.id !== id));
-    }
+// Function to remove an item
+const handleRemoveItem = (id, setState) => {
+  setState((prevItems) => prevItems.filter((item) => item.id !== id));
+};
 
-    function addLink() {
-      setLinks([
-        ...links,
-        { id: Date.now() + uuidv4(), icon: "", url: "", label: "" },
-      ]);
-    }
+function Links() {
+  const [links, setLinks] = useState([
+    { id: uuidv4(), icon: "", url: "", label: "" },
+    { id: uuidv4(), icon: "", url: "", label: "" },
+    { id: uuidv4(), icon: "", url: "", label: "" },
+  ]);
 
-    function inputChange(id, field, value) {
-      setLinks(
-        links.map((link) =>
-          link.id === id ? { ...link, [field]: value } : link,
-        ),
-      );
-    }
-
-    return (
-      <>
-        <CardHeader className="flex gap-3">
-          <FaLink className="icon size-8" />
-          <div className="flex flex-col">
-            <h1 className="text-md">Links</h1>
-            <p className="text-small text-default-500">Input your socials</p>
+  return (
+    <Details heading="Links" subheading="Input your links" icon={FaLink}>
+      {links.map((link) => (
+        <Fragment key={link.id}>
+          <div className="flex w-full flex-wrap items-center md:flex-nowrap mb-4 gap-4">
+            <Input
+              type="text"
+              label="Icon"
+              value={link.icon}
+              onChange={(e) =>
+                handleInputChange(link.id, "icon", e.target.value, setLinks)
+              }
+            />
+            <Input
+              type="url"
+              label="URL"
+              value={link.url}
+              onChange={(e) =>
+                handleInputChange(link.id, "url", e.target.value, setLinks)
+              }
+            />
+            <Input
+              type="text"
+              label="Label"
+              value={link.label}
+              onChange={(e) =>
+                handleInputChange(link.id, "label", e.target.value, setLinks)
+              }
+            />
+            <Button
+              isIconOnly
+              size="lg"
+              onClick={() => handleRemoveItem(link.id, setLinks)}
+            >
+              <FaTrash className="icon size-6" />
+            </Button>
           </div>
-        </CardHeader>
-        <Divider />
-        <CardBody className="overflow-y-auto">
-          {links.map((link) => (
-            <Fragment key={link.id}>
-              <div className="flex w-full flex-wrap items-center md:flex-nowrap mb-6 md:mb-0 gap-4">
-                <Input
-                  type="text"
-                  label="Icon"
-                  isClearable
-                  className="w-full md:w-1/3"
-                  value={link.icon}
-                  onChange={(e) => inputChange(link.id, "icon", e.target.value)}
-                />
-                <Input
-                  type="url"
-                  label="URL"
-                  value={link.url}
-                  onChange={(e) => inputChange(link.id, "url", e.target.value)}
-                />
-                <Input
-                  type="text"
-                  label="Label"
-                  isClearable
-                  value={link.label}
-                  onChange={(e) =>
-                    inputChange(link.id, "label", e.target.value)
-                  }
-                />
-                <Button
-                  isIconOnly
-                  size="lg"
-                  onClick={() => removeLink(link.id)}
-                >
-                  <FaTrash className="icon size-6" />
-                </Button>
-              </div>
-              <Spacer y={4} />
-            </Fragment>
-          ))}
-          <Button size="lg" onClick={addLink}>
-            Add
-          </Button>
-        </CardBody>
-      </>
-    );
-  }
+        </Fragment>
+      ))}
+      <Button
+        size="lg"
+        onClick={() =>
+          handleAddNewItem(setLinks, {
+            id: uuidv4(),
+            icon: "",
+            url: "",
+            label: "",
+          })
+        }
+      >
+        Add
+      </Button>
+    </Details>
+  );
+}
 
-  function Education() {
-    return (
-      <>
-        <CardHeader className="flex gap-3">
-          <UserIcon className="icon size-8" />
-          <div className="flex flex-col">
-            <h1 className="text-md">Education</h1>
-            <p className="text-small text-default-500">
-              Input your education details
-            </p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody className="overflow-y-auto">
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input type="text" label="Degree" isRequired isClearable />
-            <Input type="text" label="School" />
-          </div>
-          <Spacer y={4} />
-          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input type="text" label="Start Date" isRequired isClearable />
-            <Input type="text" label="End Date" isClearable />
-          </div>
-          <Spacer y={4} />
-          <Input type="text" label="Location" isClearable />
-          <Spacer y={4} />
-          <Textarea
-            label="Description"
-            minRows={2}
-            maxRows={4}
-            placeholder="Write a short description about your education."
-          />
-        </CardBody>
-      </>
-    );
-  }
+function Education() {
+  const educationFields = [
+    [
+      { type: "text", label: "Degree", required: true },
+      { type: "text", label: "School" },
+    ],
+    [
+      { type: "text", label: "Start Date", required: true },
+      { type: "text", label: "End Date" },
+    ],
+    [{ type: "text", label: "Location" }],
+  ];
 
-  return pages[pageIdx - 1];
+  return (
+    <Details
+      heading="Education"
+      subheading="Input your education details"
+      icon={FaGraduationCap}
+    >
+      <DetailContent fields={educationFields} />
+      <Textarea
+        label="Description"
+        minRows={2}
+        maxRows={4}
+        placeholder="Write a short description about your education."
+      />
+    </Details>
+  );
+}
+
+function Experience() {
+  const experienceFields = [
+    [
+      { type: "text", label: "Job Title", required: true },
+      { type: "text", label: "Company" },
+    ],
+    [
+      { type: "text", label: "Start Date", required: true },
+      { type: "text", label: "End Date" },
+    ],
+    [{ type: "text", label: "Location" }],
+  ];
+
+  return (
+    <Details
+      heading="Experience"
+      subheading="Input your experience details"
+      icon={FaBriefcase}
+    >
+      <DetailContent fields={experienceFields} />
+      <Textarea
+        label="Description"
+        minRows={2}
+        maxRows={4}
+        placeholder="Write a short description about your experience."
+      />
+    </Details>
+  );
+}
+
+function Skills() {
+  const [skills, setSkills] = useState([
+    { id: uuidv4(), label: "" },
+    { id: uuidv4(), label: "" },
+    { id: uuidv4(), label: "" },
+  ]);
+
+  return (
+    <Details
+      heading="Skills"
+      subheading="Input your Skills"
+      icon={FaLaptopCode}
+    >
+      {skills.map((skill) => (
+        <Fragment key={skill.id}>
+          <div className="flex w-full flex-wrap items-center md:flex-nowrap mb-4 gap-4">
+            <Input
+              type="text"
+              label="Label"
+              value={skill.label}
+              onChange={(e) =>
+                handleInputChange(skill.id, "label", e.target.value, setSkills)
+              }
+            />
+            <Button
+              isIconOnly
+              size="lg"
+              onClick={() => handleRemoveItem(skill.id, setSkills)}
+            >
+              <FaTrash className="icon size-6" />
+            </Button>
+          </div>
+        </Fragment>
+      ))}
+      <Button
+        size="lg"
+        onClick={() => handleAddNewItem(setSkills, { id: uuidv4(), label: "" })}
+      >
+        Add
+      </Button>
+    </Details>
+  );
+}
+
+function Page({ pageIdx }) {
+  const pages = useMemo(
+    () => [PersonalDetails, Links, Skills, Education, Experience ],
+    [],
+  );
+
+  const ActivePage = pages[pageIdx - 1];
+
+  return <ActivePage />;
 }
 
 function App() {
