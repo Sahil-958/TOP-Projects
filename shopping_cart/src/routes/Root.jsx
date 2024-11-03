ae4eab554f7b93e6acd4d924e5244ef9b9ab8bfd
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-11-03 17:02:33 +0530

    initail @mantine setup

--- shopping_cart/src/routes/Root.jsx
+++ shopping_cart/src/routes/Root.jsx
@@ -1,24 +1,78 @@
 import { Outlet, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { generateColors } from "@mantine/colors-generator";
+import {
+  MantineProvider,
+  ColorPicker,
+  Popover,
+  ActionIcon,
+  Select,
+  useMantineColorScheme,
+} from "@mantine/core";
+import { IoMdColorPalette } from "react-icons/io";
+import { FaSun, FaMoon, FaAdjust } from "react-icons/fa";
+
 function Root() {
+  const [accentColor, setAccentColor] = useState("rgba(47, 119, 150, 0.7)");
   return (
     <>
-      <div className="flex flex-col">
-        <header className="bg-red-100 p-4">
-          <nav>
-            <NavLink
-              to="/app"
-              className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "bg-green-50" : ""
-              }
-            >
-              Messages
-            </NavLink>
-            ;
-          </nav>
-        </header>
-        <Outlet />
-      </div>
+      <MantineProvider
+        theme={{
+          primaryColor: "custom",
+          colors: { custom: generateColors(accentColor) },
+        }}
+      >
+        <div className="flex flex-col">
+          <Popover width={300} position="bottom" withArrow shadow="md">
+            <Popover.Target>
+              <ActionIcon>
+                <IoMdColorPalette />
+              </ActionIcon>
+            </Popover.Target>
+            <Popover.Dropdown>
+              <ColorPicker
+                format="rgba"
+                fullWidth
+                value={accentColor}
+                onChange={setAccentColor}
+              />
+              <ThemeToggle />
+            </Popover.Dropdown>
+          </Popover>
+          <Outlet />
+        </div>
+      </MantineProvider>
     </>
+  );
+}
+
+import React from "react";
+
+function ThemeToggle() {
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+  console.log(colorScheme);
+  const getIcon = (scheme) => {
+    switch (scheme) {
+      case "auto":
+        return <FaAdjust />;
+      case "dark":
+        return <FaMoon />;
+      case "light":
+        return <FaSun />;
+      default:
+        return <FaAdjust />;
+    }
+  };
+  return (
+    <Select
+      leftSection={getIcon(colorScheme)}
+      leftSectionPointerEvents="none"
+      defaultValue={colorScheme}
+      data={["auto", "dark", "light"]}
+      onChange={(val) => {
+        if (val) setColorScheme(val);
+      }}
+    />
   );
 }
 
