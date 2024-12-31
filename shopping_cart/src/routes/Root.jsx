6b07eac3a248aa203460e231bb50f2076eb9c8e2
Sahil <118348625+Sahil-958@users.noreply.github.com>
2024-12-31 20:29:12 +0530

    restaring the work on project

--- shopping_cart/src/routes/Root.jsx
+++ shopping_cart/src/routes/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { generateColors } from "@mantine/colors-generator";
 import {
   MantineProvider,
@@ -13,7 +13,12 @@ import {
   AppShell,
   Burger,
   Group,
-  TextInput,
+  Skeleton,
+  Flex,
+  Avatar,
+  Title,
+  Stack,
+  Autocomplete,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IoMdColorPalette } from "react-icons/io";
@@ -24,6 +29,7 @@ import "@mantine/core/styles.css";
 function Root() {
   const [accentColor, setAccentColor] = useState("rgba(47, 119, 150, 0.7)");
   const [opened, { toggle }] = useDisclosure();
+
   return (
     <>
       <MantineProvider
@@ -52,14 +58,7 @@ function Root() {
                 />
                 <SiFdroid size={20} />
               </Group>
-              <TextInput
-                variant="filled"
-                size="xs"
-                radius="md"
-                leftSectionPointerEvents="none"
-                leftSection={<FaSearch />}
-                placeholder="App search"
-              />
+              <Search />
             </Group>
           </AppShell.Header>
           <AppShell.Navbar p="md">
@@ -90,9 +89,126 @@ function Root() {
   );
 }
 
+function Search() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchSuggestionLabel, setSearchSuggestionLabel] = useState(
+    Array.from({ length: 5 }, (_, i) => `Suggestion ${i + 1}`),
+  );
+  const [searchSuggestion, setSearchSuggestion] = useState({});
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    console.log("fetching data");
+    const fetchData = async () => {
+      try {
+        await fetch(
+          `https://dummyjson.com/products/search?q=${searchValue}&limit=5&delay=2000`,
+          { signal: controller.signal },
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setSearchSuggestion({
+              ...data,
+            });
+            setSearchSuggestionLabel([
+              {
+                group: `${data.total} results found for Term: "${searchValue}"`,
+                items: data.products.map((product) => product.description),
+              },
+            ]);
+          });
+        console.log("fetched");
+      } catch (err) {
+        console.log("Aborted");
+        return;
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+    return () => controller.abort();
+  }, [searchValue]);
+
+  function optionsFilter({ options }) {
+    return options; //filtering is happeing via api so just return the original
+  }
+
+  function getSerachTermSuggestionCard(res) {
+    const product = searchSuggestion?.products?.find(
+      (product) => product.description === res.option.value,
+    );
+    return (
+      <>
+        <Flex
+          className="w-full"
+          direction={"row"}
+          gap={5}
+          justify={"space-between"}
+          align={"center"}
+        >
+          <Skeleton height={70} width={60} radius="sm" visible={isLoading}>
+            <Avatar src={product?.images[0]} size={56} radius="xl" />
+          </Skeleton>
+          <Stack gap={5} className="w-full">
+            <Skeleton
+              height={20}
+              width={"100%"}
+              radius="sm"
+              visible={isLoading}
+            >
+              <Title size="md" textWrap="pretty">
+                {product?.title}
+              </Title>
+            </Skeleton>
+            <Skeleton
+              height={45}
+              width={"100%"}
+              radius="sm"
+              visible={isLoading}
+            >
+              <Title
+                truncate={"end"}
+                lineClamp={2}
+                size="sm"
+                c="dimmed"
+                textWrap="pretty"
+              >
+                {product?.description}
+              </Title>
+            </Skeleton>
+          </Stack>
+        </Flex>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Autocomplete
+        value={searchValue}
+        onChange={setSearchValue}
+        variant="filled"
+        size="sm"
+        radius="md"
+        maxDropdownHeight={800}
+        data={searchSuggestionLabel}
+        filter={optionsFilter}
+        renderOption={getSerachTermSuggestionCard}
+        leftSectionPointerEvents="none"
+        leftSection={<FaSearch />}
+        placeholder="App search"
+      />
+    </>
+  );
+}
+
 function ThemeToggle() {
-  const { colorScheme, setColorScheme } = useMantineColorScheme();
-  console.log(colorScheme);
+  const { colorScheme, setColorScheme } = useMantineColorScheme({
+    keepTransitions: true,
+  });
   const getIcon = (scheme) => {
     switch (scheme) {
       case "auto":
