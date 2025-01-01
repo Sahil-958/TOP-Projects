40dc82e64e2433367ec60a394261846a650f19ef
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-01 13:40:55 +0530

    added search path

--- shopping_cart/src/main.jsx
+++ shopping_cart/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import SearchPage from "./routes/SearchPage.jsx";
 import Root from "./routes/Root.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -9,7 +10,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "app", element: <App /> }],
+    children: [
+      {
+        path: "app",
+        element: <App />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
   },
 ]);
 
