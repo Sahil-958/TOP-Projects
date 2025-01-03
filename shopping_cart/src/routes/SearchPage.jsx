3cc32226dd3ce1ba6dd05c6d2a8b2a92705db579
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-03 14:08:56 +0530

    search page now fetchs new products on scroll

--- shopping_cart/src/routes/SearchPage.jsx
+++ shopping_cart/src/routes/SearchPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 import { useSearchParams } from "react-router";
 import { useFetchProducts } from "../hooks/useFetchProducts.jsx";
 import {
@@ -12,99 +13,146 @@ import {
   Badge,
   Button,
   Group,
+  ScrollArea,
+  Divider,
+  useMantineTheme,
 } from "@mantine/core";
 
 const SearchPage = () => {
+  let navigate = useNavigate();
   let [searchParams] = useSearchParams();
-  let query = searchParams.get("q");
-  let limit = searchParams.get("limit");
-  let delay = searchParams.get("delay");
-  let total = searchParams.get("total");
-  let { data, loading, error } = useFetchProducts(
-    `https://dummyjson.com/products/search?q=${query || ""}&limit=${limit || 10}&delay=${delay || 5000}`,
-  );
-  console.log("total", total, loading);
+  let query = searchParams.get("q") || "";
+  let limit = Number(searchParams.get("limit")) || 10;
+  let delay = Number(searchParams.get("delay")) || 5000;
+  let skip = Number(searchParams.get("skip")) || 0;
+  let url = `https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}&delay=${delay}`;
 
-  const [imageLoaded, setImageLoaded] = useState({}); // Track loaded state for each image
+  let { data, error, loading } = useFetchProducts(url);
 
-  // Handle when an image has loaded
-  const handleImageLoad = (id) => {
-    setImageLoaded((prev) => ({
-      ...prev,
-      [id]: true, // Mark image as loaded
-    }));
+  const fetchNextPage = () => {
+    if (loading) return;
+    let upSkip = skip + limit > data.total ? data?.total - limit : skip + limit;
+    console.log("Fetching next page...", skip, upSkip, data?.total);
+    navigate(
+      `/search?q=${query}&limit=${limit}&skip=${upSkip}&delay=${delay}`,
+      { replace: true },
+    );
   };
-
+  const theme = useMantineTheme();
   return (
     <>
-      <Grid>
-        {loading
-          ? Array.from(
-              { length: total && total < 10 && total > 0 ? total : 10 },
-              (_, i) => (
-                <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={i}>
+      <ScrollArea
+        p={"md"}
+        styles={{
+          root: {
+            height: "calc(100vh - var(--app-shell-header-height))",
+          },
+        }}
+        onBottomReached={fetchNextPage}
+      >
+        <Divider
+          mb="md"
+          label=<Text size="xs">
+            {loading
+              ? "Hang Tight While We Fetch Products..."
+              : `${data.total} Products found for Term: `}
+            <Text
+              fw={700}
+              tt={"capitalize"}
+              size="md"
+              span
+              c={theme.primaryColor}
+            >
+              {!loading && query}
+            </Text>
+          </Text>
+          labelPosition="center"
+        />
+        <Grid>
+          {loading
+            ? Array.from(
+                { length: data?.total > 10 ? 10 : data?.total && 10 },
+                (_, i) => (
+                  <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={i}>
+                    <Card
+                      //h={"100%"}
+                      h={400}
+                      shadow="sm"
+                      padding="lg"
+                      radius="md"
+                      withBorder
+                      key={i}
+                    >
+                      <Card.Section>
+                        <Skeleton h={200} radius="sm" />
+                      </Card.Section>
+                      <Group justify="space-between" mt={"md"}>
+                        <Skeleton height={20} width={"70%"} radius="sm" />
+                        <Skeleton height={20} width={"20%"} radius="xl" />
+                      </Group>
+                      <Skeleton height={15} radius="xs" mt={"xl"} />
+                      <Skeleton
+                        height={15}
+                        width={"80%"}
+                        radius="xs"
+                        mt={"xs"}
+                      />
+                      <Skeleton height={35} radius="xs" mt={"md"} />
+                    </Card>
+                  </Grid.Col>
+                ),
+              )
+            : data?.products?.map((product) => (
+                <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={product.id}>
                   <Card
-                    h={"100%"}
+                    h={400}
                     shadow="sm"
                     padding="lg"
                     radius="md"
                     withBorder
-                    key={i}
+                    key={product.id}
                   >
                     <Card.Section>
-                      <Skeleton h={200} radius="sm" />
+                      <ProductImage
+                        src={product.images[0]}
+                        title={product.title}
+                      />
                     </Card.Section>
-                    <Group justify="space-between" mt={"md"}>
-                      <Skeleton height={20} width={"70%"} radius="sm" />
-                      <Skeleton height={20} width={"20%"} radius="xl" />
-                    </Group>
-                    <Skeleton height={15} radius="xs" mt={"xl"} />
-                    <Skeleton height={15} width={"80%"} radius="xs" mt={"xs"} />
-                    <Skeleton height={35} radius="xs" mt={"md"} />
+                    <Stack h={"100%"} justify="end" gap={"sm"}>
+                      <Group justify="space-between" mt="md" mb="xs">
+                        <Title size="md" textWrap="wrap" lineClamp={1}>
+                          {product.title}
+                        </Title>
+                        <Badge>On Sale</Badge>
+                      </Group>
+                      <Text size="sm" c="dimmed" lineClamp={2}>
+                        {product.description}
+                      </Text>
+                      <Button fullWidth radius="md">
+                        Click
+                      </Button>
+                    </Stack>
                   </Card>
                 </Grid.Col>
-              ),
-            )
-          : data?.products?.map((product) => (
-              <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={product.id}>
-                <Card
-                  h={"100%"}
-                  shadow="sm"
-                  padding="lg"
-                  radius="md"
-                  withBorder
-                  key={product.id}
-                >
-                  <Card.Section>
-                    <Skeleton visible={!imageLoaded[product.id]}>
-                      <Image
-                        onLoad={() => handleImageLoad(product.id)}
-                        src={product.images[0]}
-                        h={200}
-                        fit="contain"
-                        alt={product.title}
-                      />
-                    </Skeleton>
-                  </Card.Section>
-                  <Stack h={"100%"} justify="end" gap={"sm"}>
-                    <Group justify="space-between" mt="md" mb="xs">
-                      <Title size="md" textWrap="wrap" lineClamp={1}>
-                        {product.title}
-                      </Title>
-                      <Badge>On Sale</Badge>
-                    </Group>
-                    <Text size="sm" c="dimmed" lineClamp={2}>
-                      {product.description}
-                    </Text>
-                    <Button fullWidth radius="md">
-                      Click
-                    </Button>
-                  </Stack>
-                </Card>
-              </Grid.Col>
-            ))}
-      </Grid>
+              ))}
+        </Grid>
+      </ScrollArea>
     </>
+  );
+};
+
+const ProductImage = ({ src, title }) => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <Skeleton visible={loading}>
+      <Image
+        onLoad={() => setLoading(false)}
+        src={src}
+        h={200}
+        fit="contain"
+        alt={title}
+      />
+    </Skeleton>
   );
 };
 
