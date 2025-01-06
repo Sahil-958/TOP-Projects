7e363cabe549d2f008db17c389d170a3e7fc76be
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-06 09:20:51 +0530

    refactored infinite scrolling to standalone component

---
+++ shopping_cart/src/components/InfiniteScroll.jsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Grid, Text, ScrollArea, Divider, Loader } from "@mantine/core";
+import ProductCard from "../components/ProductCard.jsx";
+import SkeletonProductCard from "../components/SkeletonProductCard.jsx";
+
+
+export default function InfiniteScroll({
+  clearDeps = [],
+  query,
+  skip = 0,
+  data,
+  loading,
+  nextPageUrlGenerator,
+  error,
+}) {
+  let total = data?.total || 10;
+  const [productsMap, setProductsMap] = useState(new Map());
+
+  useEffect(() => {
+    productsMap.clear();
+    console.log("clear");
+  }, clearDeps);
+
+  useEffect(() => {
+    if (data?.products) {
+      setProductsMap((prev) => {
+        const newMap = new Map(prev); // Clone existing map
+        data.products.forEach((product) => {
+          if (!newMap.has(product.id)) {
+            newMap.set(product.id, product); // Add unique products
+          }
+        });
+        return newMap;
+      });
+    }
+    console.log("map");
+  }, [data]);
+  const products = Array.from(productsMap.values());
+
+  const navigate = useNavigate();
+  const fetchNextPage = () => {
+    if (loading) return;
+    console.log("fetch next page");
+    navigate(
+      nextPageUrlGenerator(),
+      { replace: true },
+    );
+  };
+  return (
+    <ScrollArea
+      offsetScrollbars="y"
+      p={"md"}
+      styles={{
+        root: {
+          height: "calc(100vh - var(--app-shell-header-height))",
+        },
+      }}
+      onBottomReached={fetchNextPage}
+    >
+      <Divider
+        mb="md"
+        label=<Text size="sm">
+          <Text
+            fw={700}
+            tt={"capitalize"}
+            size="md"
+            span
+            c="var(--mantine-primary-color-filled)"
+          >
+            {!loading && total}
+          </Text>
+          {loading ? (
+            <Loader
+              color={"var(--mantine-primary-color-filled)"}
+              type="dots"
+            />
+          ) : (
+            ` Products found for Term: `
+          )}
+          <Text
+            fw={700}
+            tt={"capitalize"}
+            size="md"
+            span
+            c="var(--mantine-primary-color-filled)"
+          >
+            {!loading && query}
+          </Text>
+        </Text>
+        labelPosition="center"
+      />
+      <Grid grow>
+        {products?.map((product) => (
+          <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={product.id}>
+            <ProductCard product={product} />
+          </Grid.Col>
+        ))}
+        {loading &&
+          Array.from(
+            { length: total > 10 ? 10 : total || 10 },
+            (_, i) => (
+              <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={i}>
+                <SkeletonProductCard key={i} />
+              </Grid.Col>
+            ),
+          )}
+      </Grid>
+      {!loading && total - skip - 1 == 0 && (
+        <Divider mt="md" label={"No more Products"} labelPosition="center" />
+      )}
+    </ScrollArea>)
+    ;
+}
+
+
--- shopping_cart/src/routes/SearchPage.jsx
+++ shopping_cart/src/routes/SearchPage.jsx
@@ -1,118 +1,27 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
 import { useSearchParams } from "react-router";
-import { useFetchProducts } from "../hooks/useFetchProducts.jsx";
-import { Grid, Text, ScrollArea, Divider, Loader } from "@mantine/core";
-import ProductCard from "../components/ProductCard.jsx";
-import SkeletonProductCard from "../components/SkeletonProductCard.jsx";
+import { useFetchProducts } from "../hooks/useFetchProducts";
+import InfiniteScroll from "../components/InfiniteScroll.jsx";
 
 const SearchPage = () => {
-  const navigate = useNavigate();
   let [searchParams] = useSearchParams();
   let query = searchParams.get("q") || "";
   let limit = Number(searchParams.get("limit")) || 10;
   let delay = Number(searchParams.get("delay")) || 5000;
   let skip = Number(searchParams.get("skip")) || 0;
-  let url = `https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}&delay=${delay}`;
-  let { data, error, loading } = useFetchProducts(url, true);
+  let { data, error, loading } = useFetchProducts({
+    url: `https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}&delay=${delay}`,
+    localRequest: false,
+  });
 
-  const [productsMap, setProductsMap] = useState(new Map());
-  useEffect(() => {
-    productsMap.clear();
-  }, [query]);
-
-  useEffect(() => {
-    if (data?.products) {
-      setProductsMap((prev) => {
-        const newMap = new Map(prev); // Clone existing map
-        data.products.forEach((product) => {
-          if (!newMap.has(product.id)) {
-            newMap.set(product.id, product); // Add unique products
-          }
-        });
-        return newMap;
-      });
-    }
-  }, [data]);
-
-  const products = Array.from(productsMap.values()); // Convert Map to Array for rendering
-
-  const fetchNextPage = () => {
-    if (loading) return;
-    let upSkip =
-      skip + limit >= data?.total
-        ? skip + (data?.total - skip - 1)
-        : skip + limit;
-    console.log("Fetching next page...", skip, upSkip, data?.total);
-    navigate(
-      `/search?q=${query}&limit=${limit}&skip=${upSkip}&delay=${delay}`,
-      { replace: true },
-    );
+  function nextPageUrl() {
+    let nextSkip = Math.min(skip + limit, data?.total - 1);
+    return `/search?q=${query}&limit=${limit}&skip=${nextSkip}&delay=${delay}`;
   };
+
+
   return (
     <>
-      <ScrollArea
-        offsetScrollbars="y"
-        p={"md"}
-        styles={{
-          root: {
-            height: "calc(100vh - var(--app-shell-header-height))",
-          },
-        }}
-        onBottomReached={fetchNextPage}
-      >
-        <Divider
-          mb="md"
-          label=<Text size="sm">
-            <Text
-              fw={700}
-              tt={"capitalize"}
-              size="md"
-              span
-              c="var(--mantine-primary-color-filled)"
-            >
-              {!loading && data.total}
-            </Text>
-            {loading ? (
-              <Loader
-                color={"var(--mantine-primary-color-filled)"}
-                type="dots"
-              />
-            ) : (
-              ` Products found for Term: `
-            )}
-            <Text
-              fw={700}
-              tt={"capitalize"}
-              size="md"
-              span
-              c="var(--mantine-primary-color-filled)"
-            >
-              {!loading && query}
-            </Text>
-          </Text>
-          labelPosition="center"
-        />
-        <Grid grow>
-          {products?.map((product) => (
-            <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={product.id}>
-              <ProductCard product={product} />
-            </Grid.Col>
-          ))}
-          {loading &&
-            Array.from(
-              { length: data?.total > 10 ? 10 : data?.total || 10 },
-              (_, i) => (
-                <Grid.Col span={{ base: 12, md: 6, lg: 3 }} key={i}>
-                  <SkeletonProductCard key={i} />
-                </Grid.Col>
-              ),
-            )}
-        </Grid>
-        {!loading && data?.total - skip - 1 == 0 && (
-          <Divider mt="md" label={"No more Products"} labelPosition="center" />
-        )}
-      </ScrollArea>
+      <InfiniteScroll nextPageUrlGenerator={nextPageUrl} error={error} query={query} clearDeps={[query]} data={data} loading={loading} />
     </>
   );
 };
