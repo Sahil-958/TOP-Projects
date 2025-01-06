0e2ce0dd10e269955b9d6abe2909b17f5a56a8c6
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-06 09:21:07 +0530

    added categories page

---
+++ shopping_cart/src/routes/Categories.jsx
@@ -0,0 +1,26 @@
+import { useParams, useSearchParams } from "react-router";
+import { useFetchProducts } from "../hooks/useFetchProducts";
+import InfiniteScroll from "../components/InfiniteScroll.jsx";
+
+export default function Categories() {
+  let { category } = useParams();
+  let [searchParams] = useSearchParams();
+  let limit = Number(searchParams.get("limit")) || 10;
+  let delay = Number(searchParams.get("delay")) || 5000;
+  let skip = Number(searchParams.get("skip")) || 0;
+  let { data, error, loading } = useFetchProducts({
+    url: `https://dummyjson.com/products/category/${category}?limit=${limit}&skip=${skip}&dealy=${delay}`,
+  });
+
+  function nextPageUrl() {
+    let nextSkip = Math.min(skip + limit, data?.total - 1);
+    return `/categories/${category}?limit=${limit}&skip=${nextSkip}&delay=${delay}`;
+  }
+
+  return (
+    <>
+      <InfiniteScroll nextPageUrlGenerator={nextPageUrl} error={error} query={category} clearDeps={[category]} data={data} loading={loading} />
+    </>
+  );
+};
+
