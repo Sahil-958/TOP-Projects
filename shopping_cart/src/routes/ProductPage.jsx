70cca081515892dd6483a20b92acfaebc10f51c1
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-07 19:09:37 +0530

    added fallback fetch logic and UI changes

--- shopping_cart/src/routes/ProductPage.jsx
+++ shopping_cart/src/routes/ProductPage.jsx
@@ -1,6 +1,8 @@
 import {
   Stack,
+  Divider,
   Card,
+  Avatar,
   Image,
   Text,
   Title,
@@ -8,48 +10,61 @@ import {
   Button,
   Group,
   Grid,
+  Flex,
   ThemeIcon,
   ActionIcon,
   ScrollArea,
+  Skeleton,
   Spoiler,
 } from "@mantine/core";
 import { Carousel } from '@mantine/carousel';
 import Autoplay from 'embla-carousel-autoplay';
 import { LuStar, LuPackagePlus } from "react-icons/lu";
-import { CiBoxes } from "react-icons/ci";
 import { TbTruckReturn, TbShieldCheck, TbWeight, TbPackage, TbCategory } from "react-icons/tb";
 import { RxDimensions } from "react-icons/rx";
 import { MdAddShoppingCart } from "react-icons/md";
-import { LiaShippingFastSolid } from "react-icons/lia";
+import { LiaShippingFastSolid, LiaBoxesSolid } from "react-icons/lia";
 import { useParams, useLocation } from "react-router";
 import { useRef } from "react";
+import { formatDistanceToNow } from "date-fns";
 import { useFetchProducts } from "../hooks/useFetchProducts";
 
 export default function ProductPage() {
   let { id } = useParams();
   const location = useLocation();
-  const product = location.state.product;
+  let product, data, loading, error;
+  if (location?.state?.product) {
+    product = location.state.product;
+    loading = false;
+    error = null;
+  } else {
+    ({ data, loading, error } = useFetchProducts({
+      url: `https://dummyjson.com/products/${id}?delay=5000`
+    }
+    ))
+    product = data;
+  }
   const autoplay = useRef(Autoplay({ delay: 3000 }));
 
   let productInfo = [
-    { icon: TbTruckReturn, text: product.returnPolicy, color: "blue" },
-    { icon: TbShieldCheck, text: product.warrantyInformation, color: "green" },
-    { icon: LiaShippingFastSolid, text: product.shippingInformation, color: "teal" },
+    { icon: TbTruckReturn, text: loading || product.returnPolicy, color: "blue" },
+    { icon: TbShieldCheck, text: loading || product.warrantyInformation, color: "green" },
+    { icon: LiaShippingFastSolid, text: loading || product.shippingInformation, color: "teal" },
     {
       icon: TbPackage,
-      text: `Minimum Order: ${product.minimumOrderQuantity}`,
+      text: loading || `Minimum Order: ${product.minimumOrderQuantity}`,
       color: "violet"
     },
     {
       icon: TbCategory,
-      text: `${product.category}`,
+      text: loading || `${product.category}`,
       color: "cyan"
     },
-    { icon: CiBoxes, text: `In Stock: ${product.stock}`, color: "red" },
-    { icon: TbWeight, text: `${product.weight}`, color: "orange" },
+    { icon: LiaBoxesSolid, text: loading || `In Stock: ${product.stock}`, color: "red" },
+    { icon: TbWeight, text: loading || `${product.weight}`, color: "orange" },
     {
       icon: RxDimensions,
-      text: `${product.dimensions.width} x ${product.dimensions.height} x ${product.dimensions.depth}`,
+      text: loading || `${product.dimensions.width} x ${product.dimensions.height} x ${product.dimensions.depth}`,
       color: "grape"
     },
 
@@ -58,109 +73,124 @@ export default function ProductPage() {
   return (
     <ScrollArea
       offsetScrollbars="y"
-      scrollbarSize={8}
+      pr={10}
       styles={{
         root: {
           height: "calc(100vh - var(--app-shell-header-height))",
         },
       }}
     >
-      <Card
-        h={"100%"}
-        shadow="sm"
-        mb="xl"
-        padding="lg"
-        radius="md"
-        withBorder
-        key={product.id}
-      >
-        <Card.Section >
+      <Stack>
+        <Skeleton h={300} visible={loading}>
           <Carousel h={300} withIndicators loop slideSize={"70%"} slideGap={10}
             speed={3}
             plugins={[autoplay.current]}
-            //onSlideChange
             onMouseEnter={autoplay.current.stop}
             onMouseLeave={autoplay.current.reset}
 
           >
-            {product.images.map((url) => (
+            {loading || product.images.map((url) => (
               <Carousel.Slide key={url} >
-                <Image radius={"md"} src={url} h={300} fit="contain" alt={product.title} />
+                <Image radius={"md"} src={url} h={300} fit="contain" alt={loading || product.title} />
               </Carousel.Slide>
             ))
             }
           </Carousel>
-        </Card.Section>
-        <Stack h={"100%"} justify="end" gap={"sm"}>
-          <div>
-            <Group gap={0}>
-              <Text size="xs" c="dimmed" lineClamp={2}>
-                {product.brand}
-              </Text>
-              <Badge leftSection={<LuStar />} pl={product.brand || 0} variant="transparent">
-                {product.rating}
-              </Badge>
-            </Group>
-            <Group justify="space-between" mb="xs">
-              <Title size="lg" textWrap="wrap" maw={"60%"} lineClamp={1}>
-                {product.title}
-              </Title>
-              <Badge size="lg" variant="light">
-                ${product.price}
-              </Badge>
-            </Group>
-          </div>
-          <Spoiler maxHeight={40} showLabel="...more" hideLabel="Hide">
-            <Text size="sm" >
-              {product.description}
+        </Skeleton>
+        <Stack gap={0} >
+          <Group gap={0}>
+            <Text size="xs" c="dimmed" lineClamp={2}>
+              {loading || product.brand}
             </Text>
-          </Spoiler>
-          <Group wrap="nowrap">
-            <ActionIcon.Group>
-              <ActionIcon.GroupSection variant="default" size="lg" radius="md">
-                {0}
-              </ActionIcon.GroupSection>
-              <ActionIcon
-                variant="light"
-                size="lg"
-                radius="md"
-              //onClick={increment}
-              >
-                <MdAddShoppingCart />
-              </ActionIcon>
-            </ActionIcon.Group>
-            <Button variant="light" fullWidth radius="md">
-              Buy
-            </Button>
+            <Badge leftSection={<LuStar />} pl={loading || product.brand || 0} variant="transparent">
+              {loading || product.rating}
+            </Badge>
+          </Group>
+          <Group justify="space-between" >
+            <Title size="lg" textWrap="pretty" maw={"60%"} lineClamp={1}>
+              {loading || product.title}
+            </Title>
+            <Badge size="lg" variant="light">
+              {loading || `$${product.price}`}
+            </Badge>
           </Group>
         </Stack>
-        <Card.Section withBorder mt={"md"} p={"md"}>
-          <Grid mt="lg" w="100%" justify="space-evenly" >
-            {productInfo.map((item, index) => (
-              <Grid.Col span={{ base: 3, md: 3, lg: 1 }} key={index}>
-                <Stack key={index} spacing="md" align="center">
-                  <ThemeIcon
-                    variant="light"
-                    radius="md"
-                    size="xl"
-                    color={item.color}
-                  >
-                    <item.icon size={24} />
-                  </ThemeIcon>
-                  <Text size="sm" c="dimmed" ta="center" lineClamp={2}>
-                    {item.text}
+        <Spoiler maxHeight={45} showLabel="...more" hideLabel="Hide">
+          <Text size="sm" >
+            {loading || product.description}
+          </Text>
+        </Spoiler>
+        <Group wrap="nowrap">
+          <ActionIcon.Group>
+            <ActionIcon.GroupSection variant="default" size="lg" radius="md">
+              {0}
+            </ActionIcon.GroupSection>
+            <ActionIcon
+              variant="light"
+              size="lg"
+              radius="md"
+            //onClick={increment}
+            >
+              <MdAddShoppingCart />
+            </ActionIcon>
+          </ActionIcon.Group>
+          <Button variant="light" fullWidth radius="md" disabled={loading || product.stock <= 0}>
+            {loading || product.stock > 0 ? "Buy" : "Out of Stock"}
+          </Button>
+        </Group>
+        <Divider />
+        <Grid w="100%" justify="space-evenly" >
+          {productInfo.map((item, index) => (
+            <Grid.Col span={{ base: 3, md: 3, lg: 1 }} key={index}>
+              <Stack key={index} align="center">
+                <ThemeIcon
+                  variant="light"
+                  radius="md"
+                  size="xl"
+                //color={item.color}
+                >
+                  <item.icon size={24} />
+                </ThemeIcon>
+                <Text size="sm" ta="center" lineClamp={3}>
+                  {item.text}
+                </Text>
+              </Stack>
+            </Grid.Col>
+          ))}
+        </Grid>
+        <Divider />
+        {
+          loading || product.reviews.map((review, index) => (
+            <Card shadow="sm" padding="lg" withBorder>
+              <Group position="apart" wrap="nowrap">
+                <Avatar radius="md" color="var(--mantine-primary-color-filled)" >{review.reviewerName[0]}</Avatar>
+                <Stack gap={0}>
+                  <Group gap="sm">
+                    <Title size="lg" textWrap="pretty" lineClamp={1}>
+                      {review.reviewerName}
+                    </Title>
+                    <Badge leftSection={<LuStar />} variant="light">
+                      {review.rating}
+                    </Badge>
+                  </Group>
+                  <Text size="xs" c="dimmed" textWrap="wrap" lineClamp={1}>
+                    {review.reviewerEmail}
                   </Text>
                 </Stack>
-              </Grid.Col>
-            ))}
-          </Grid>
-        </Card.Section>
-        <Card.Section withBorder mt={"md"} p={"md"}>
-          {product.reviews.map((review, index) => (
-            <Text key={index} size="sm" c="dimmed" lineClamp={2}>{review.comment}</Text>
-          ))}
-        </Card.Section>
-      </Card >
-    </ScrollArea>
+              </Group>
+              <Spoiler maxHeight={40} showLabel="...more" hideLabel="Hide">
+                <Text size="sm" mt="md">
+                  {review.comment}
+                </Text>
+              </Spoiler>
+              <Text size="xs" c="dimmed" mt="xs">
+                {formatDistanceToNow(new Date(review.date), { addSuffix: true })}
+              </Text>
+            </Card>
+          ))
+        }
+        <Divider mb="lg" label={"End of Reviews"} />
+      </Stack>
+    </ScrollArea >
   );
 }
