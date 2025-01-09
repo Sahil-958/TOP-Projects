d3568382d3dbada55f03629dd11c59d20159580b
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-01-09 20:14:49 +0530

    moved main app to app.jsx with route /app

--- shopping_cart/src/App.jsx
+++ shopping_cart/src/App.jsx
@@ -1,9 +1,110 @@
 import "./App.css";
 
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { useState } from "react";
+import { generateColors } from "@mantine/colors-generator";
+import {
+  MantineProvider,
+  ColorInput,
+  Space,
+  Paper,
+  AppShell,
+  Burger,
+  Group,
+  Slider,
+  NavLink,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { SiFdroid } from "react-icons/si";
+import { GoHome } from "react-icons/go";
+import "@mantine/core/styles.css";
+import Search from "./components/Search.jsx";
+import ThemeSwitcher from "./components/ThemeSwitcher.jsx";
+import CategoriesButton from "./components/CategoriesButton.jsx";
+
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location.pathname);
+  let color = window.localStorage.getItem("accentColor");
+  const [accentColor, setAccentColor] = useState(color || "#397e9eff");
+  const [primaryShade, setPrimaryShade] = useState(7);
+  function updateAccentColor(color) {
+    setAccentColor(color);
+    //also updaate in local storage
+    window.localStorage.setItem("accentColor", color);
+  }
+  const [opened, { toggle }] = useDisclosure();
   return (
     <>
-      <div className="bg-red-50 p-10">TEST</div>
+      <MantineProvider
+        defaultColorScheme="auto"
+        theme={{
+          primaryColor: "custom",
+          primaryShade: primaryShade,
+          colors: { custom: generateColors(accentColor) },
+        }}
+      >
+        <AppShell
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: "sm",
+            collapsed: { mobile: !opened },
+          }}
+          padding="md"
+        >
+          <AppShell.Header>
+            <Group grow h="100%" px="md">
+              <Group>
+                <Burger
+                  opened={opened}
+                  onClick={toggle}
+                  hiddenFrom="sm"
+                  size="sm"
+                />
+                <SiFdroid size={20} />
+              </Group>
+              <Search />
+            </Group>
+          </AppShell.Header>
+          <AppShell.Navbar>
+            <Paper p="md" m={"sm"} withBorder shadow="xl" radius="md">
+              <Slider
+                value={primaryShade}
+                min={0}
+                max={9}
+                step={1}
+                onChange={setPrimaryShade}
+              />
+              <Space h="xs" />
+              <ColorInput
+                variant="filled"
+                disallowInput
+                onChange={updateAccentColor}
+                defaultValue={accentColor}
+                format="hexa"
+              />
+              <Space h="xs" />
+              <ThemeSwitcher />
+            </Paper>
+            <Space h="xs" />
+            <NavLink
+              variant="light"
+              active={location.pathname === "/app"}
+              href="#"
+              key="home"
+              label="Home"
+              leftSection={<GoHome />}
+              onClick={() => navigate("/app")}
+            ></NavLink>
+            <CategoriesButton />
+          </AppShell.Navbar>
+          <AppShell.Main>
+            <Outlet />
+          </AppShell.Main>
+        </AppShell>
+      </MantineProvider>
     </>
   );
 }
--- shopping_cart/src/main.jsx
+++ shopping_cart/src/main.jsx
@@ -5,16 +5,14 @@ import App from "./App.jsx";
 import SearchPage from "./routes/SearchPage.jsx";
 import CategoriesPage from "./routes/CategoriesPage.jsx";
 import ProdcutPage from "./routes/ProductPage.jsx";
-import Root from "./routes/Root.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Root />}>
-          <Route path="app" element={<App />} />
-          <Route path="search/" element={<SearchPage />} />
+        <Route path="app" element={<App />}>
+          <Route path="search" element={<SearchPage />} />
           <Route path="categories/:category" element={<CategoriesPage />} />
           <Route path="product/:id" element={<ProdcutPage />} />
         </Route>
